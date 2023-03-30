73ad69b36e8add4eaed37f74c21151d125b5e3f4
Servideo <107768096+Servideo@users.noreply.github.com>
2023-03-30 18:47:38 -0500

    habilitacion de js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 import { LinkedList } from "./module/linkedlist.js";
 
 function main() {
-  // let llist = new LinkedList();
-  // try
-  // {
-  //   let element = '1'.split('');
-  //   if (element.length == 0) throw new Error('No se reciben espacios en blanco')
-  //   element.map(llist.insertEnd);
-  //   console.log(`lista sin invertir\n${llist.print()}`);
-  //   console.log(`tamaño ${llist.size}`)
-  //   llist.head = llist.reverse(llist.head)
-  //   console.log(`lista invertida ${llist}`);
-  // }catch(e)
-  // {
-  //   console.log(e);
-  // }
-  console.log('pepe');
+  let llist = new LinkedList();
+  try
+  {
+    let element = '1'.split('');
+    if (element.length == 0) throw new Error('No se reciben espacios en blanco')
+    element.map(llist.insertEnd);
+    console.log(`lista sin invertir\n${llist.print()}`);
+    console.log(`tamaño ${llist.size}`)
+    llist.head = llist.reverse(llist.head)
+    console.log(`lista invertida ${llist}`);
+  }catch(e)
+  {
+    console.log(e);
+  }
 }
 
 main();
